dd7bcd7ccc5ebfd7a4a88c669402218b8ea36c2a
Brian Kichler <brian.kichler@gmail.com>
2018-05-19 20:19:12 +0000

    Installed faker gem, build up seeds.rb to create random entries in db

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :classification, :type, :year)
+  end
+end
