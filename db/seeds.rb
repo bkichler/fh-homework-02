a818be746776ccaa476ff168bb3e91422c7cacd9
Brian Kichler <brian.kichler@gmail.com>
2018-06-12 02:58:48 +0000

    Created seed class to seed authors, books, and authorships

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,15 +32,48 @@
 #   t.string   "sub_title"
 # end
 
-50.times do |i|
-  book = Book.create!(:title => Faker::Book.title, :genre => Faker::Book.genre, 
-              :classification => Faker::Book.classification, :booktype => Faker::Book.booktype, 
-              :year => Faker::Date.between(50.years.ago, Date.today).strftime("%Y"), :sub_title => Faker::ChuckNorris.fact)
-  puts "Book #{i + 1}: #{book.title} created!"
+# nodoc
+class Seed
+  def self.start
+    seed = Seed.new
+    seed.create_books
+    seed.create_authors
+    seed.create_authorships
+  end
+
+  def create_books
+    50.times do |i|
+      book = Book.create!(:title => Faker::Book.title, :genre => Faker::Book.genre, 
+                  :classification => Faker::Book.classification, :booktype => Faker::Book.booktype, 
+                  :year => Faker::Date.between(50.years.ago, Date.today).strftime("%Y"), :sub_title => Faker::ChuckNorris.fact)
+      puts "Book #{i + 1}: #{book.title} created!"
+    end
+  end
+
+  def create_authors
+    25.times do |i|
+      author = Author.create!(:first_name => Faker::Name.first_name, 
+                              :last_name => Faker::Name.last_name, :age => Faker::Number.between(18, 100))
+      puts "Author #{i + 1}: #{author.first_name} #{author.last_name} created!"
+    end
+  end
+
+  def create_authorships
+    books = Book.all.ids
+    authors = Author.all.ids
+
+    associations = []
+    books.each do |book|
+      authors.each do |author|
+        associations << [book, author]
+      end
+    end
+
+    50.times do |i|
+      authorship = Authorship.create!(:book_id => associations.sample[0], :author_id => associations.sample[1])
+      puts "Authorship #{i + 1}: #{authorship.id} created!"
+    end
+  end
 end
 
-25.times do |i|
-  author = Author.create!(:first_name => Faker::Name.first_name, 
-                          :last_name => Faker::Name.last_name, :age => Faker::Number.between(18, 100))
-  puts "Author #{i + 1}: #{author.first_name} #{author.last_name} created!"
-end
+Seed.start
